b700c9ff1b8ccd54d3decd0ea576777fe47fb5e1
Noe Gallardo <noegallardo.silva@gmail.com>
2021-09-17 17:39:12 -0500

    inicio de Angular

---
+++ VentaReal/app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { ClienteComponent } from './cliente/cliente.component';
+import { MatSidenavModule } from '@angular/material/sidenav'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    ClienteComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSidenavModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
